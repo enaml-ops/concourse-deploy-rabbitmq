3d18ef839c99b9482434132cf53d4501985b3f23
Mike Jacobi <michael.jacobi@altoros.com>
2016-09-16 16:51:06 -0700

    Update vault hash in generate manifest.
    [#129557887]

--- ci/tasks/generate-pivotal-rabbitmq-service-manifest.sh
+++ ci/tasks/generate-pivotal-rabbitmq-service-manifest.sh
@@ -7,10 +7,10 @@ omg-cli/omg-linux register-plugin \
   -pluginpath omg-product-bundle/$PRODUCT_PLUGIN
 
 omg-cli/omg-linux deploy-product \
-  --bosh-url $(vault read -field=bosh-url $VAULT_HASH_HOSTVARS) \
-  --bosh-port $(vault read -field=bosh-port $VAULT_HASH_HOSTVARS) \
-  --bosh-user $(vault read -field=bosh-user $VAULT_HASH_HOSTVARS) \
-  --bosh-pass $(vault read -field=bosh-pass $VAULT_HASH_HOSTVARS) \
+  --bosh-url $(vault read -field=bosh-url $VAULT_HASH_MISC) \
+  --bosh-port $(vault read -field=bosh-port $VAULT_HASH_MISC) \
+  --bosh-user $(vault read -field=bosh-user $VAULT_HASH_MISC) \
+  --bosh-pass $(vault read -field=bosh-pass $VAULT_HASH_MISC) \
   --print-manifest \
   --ssl-ignore \
   $PRODUCT_PLUGIN \
