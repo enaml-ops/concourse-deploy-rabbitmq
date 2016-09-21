adf4674d636f4f744d548ffa2ff16f0b883f37fc
Mike Jacobi <michael.jacobi@altoros.com>
2016-09-21 14:05:30 -0700

    Update manifest generation.
    [#129557887]

--- ci/tasks/generate-pivotal-rabbitmq-service-manifest.sh
+++ ci/tasks/generate-pivotal-rabbitmq-service-manifest.sh
@@ -14,15 +14,14 @@ omg-cli/omg-linux deploy-product \
   --print-manifest \
   --ssl-ignore \
   $PRODUCT_PLUGIN \
-  --stemcell-ver $STEMCELL_VERSION \
   --infer-from-cloud \
-  --vault-active \
+  --skip-ssl-verify \
+  --stemcell-version $STEMCELL_VERSION \
   --vault-domain $VAULT_ADDR \
-  --vault-hash-host $VAULT_HASH_HOSTVARS \
-  --vault-hash-ip $VAULT_HASH_IP \
-  --vault-hash-keycert $VAULT_HASH_KEYCERT \
-  --vault-hash-misc $VAULT_HASH_MISC \
-  --vault-hash-password $VAULT_HASH_PASSWORD \
+  --vault-hash $VAULT_HASH_IP \
+  --vault-hash $VAULT_HASH_PASSWORD \
+  --vault-hash $VAULT_HASH_HOSTVARS \
+  --vault-hash $VAULT_HASH_MISC \
   --vault-token $VAULT_TOKEN > manifest/deployment.yml
 
 #eof
